556e286b9056a907b9af9e2759508fc663fe7bfa
Chien-Chin Huang <chienchin@fb.com>
2025-01-09 13:33:07 -0800

    Update
    [ghstack-poisoned]

--- torchft/fsdp_test.py
+++ torchft/fsdp_test.py
@@ -5,9 +5,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import os
-from concurrent.futures import ThreadPoolExecutor
 from typing import Any, Dict, Tuple
-from unittest import TestCase, skipUnless
 from unittest.mock import Mock
 
 import torch
